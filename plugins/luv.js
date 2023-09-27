9950b15f0c6f6e2ab05538f4a87161a031f6894c
itzNightmare17 <142595429+itzNightmare17@users.noreply.github.com>
2023-09-27 21:31:04 +0530

    Update luv.js

--- plugins/luv.js
+++ plugins/luv.js
@@ -1,8 +1,4 @@
-/* Copyright (C) 2022 Sourav KL11.
-Licensed under the  GPL-3.0 License;
-you may not use this file except in compliance with the License.
-Raganork MD - Sourav KL11
-*/
+// Oreo
 const {
   FancyRandom,
   getListFromCommand,
